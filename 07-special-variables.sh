5e0320b3bdf89ff1437129021aaf6bb5e74a45e5
rakesh <rakeshgautam602@gmail.com>
2026-09-06 18:37:40 +0530

    action

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "all variables passed to script:$@"
-echo "number of variables passed to script: $#"
+echo "all variables passed to script: $@"
+echo "number of variables passed to script: @#"
 echo "script name: $0"
 echo "present working directory: $PWD"
 echo "user running the script: $USER"
@@ -9,3 +9,4 @@ echo "current home directory: $HOME"
 echo "process instance ID of current process: $$"
 sleep 10 &
 echo "background PID: $!"
+echo "exit code of previous command: $?"
