f84515697a57c828de5fb4bebd61a11dc6b8cb32
rakesh <rakeshgautam602@gmail.com>
2026-09-07 12:37:58 +0530

    action

---
+++ 13-logs.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+LOG_FOLDER="/var/log/shell_script"
+LOG_FILE="/var/log/shell_script/$0/log"
+
+USER_ID=(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+  echo "Run this script as Root User" | tee -a $LOG_FILE
+  exit 1
+fi
+
+mkdir -p /var/log/shell_script
+
+VALIDATE(){
+  if [ $1 -ne 0 ]; then
+    echo "$2 FAILURE" | tee -a $LOG_FILE
+  else 
+    echo "$2 SUCCESS" | tee -a $LOG_FILE
+  fi
+}
+
+for PACKAGE in $@
+do 
+  dnf list installed $PACKAGE | tee -a $LOG_FILE
+  if [ $? -ne 0 ]; then
+    echo "$PAKCAGE not installed installing now"
+    dnf install $PACKAGE -y &>> $LOG_FILE
+    VALIDATE $? "$PACKAGE installation"
+  else 
+    echo "$PACKAGE already installed skipping"
+  fi
+done
