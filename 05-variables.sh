41d7d969ec4cc2f6441ef1cc3448c813101293ca
rakesh <rakeshgautam602@gmail.com>
2026-09-07 10:07:22 +0530

    action

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-echo "Please enter your username"
-read "USER_NAME"
-
-echo "user name is $USER_NAME"
-
-echo "Please enter your password"
-read -s "PASSWORD"
-
-echo "password is $PASSWORD"
 
