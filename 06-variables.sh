467ba975750e1fda80a2d43c4ff460bb66565ec8
rakesh <rakeshgautam602@gmail.com>
2026-09-07 10:15:30 +0530

    action

--- 06-variables.sh
+++ 06-variables.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
+START_TIME=$(date +%s)
+echo "script executed at $START_TIME"
 
+sleep 10
+
+END_TIME=$(date +%s)
+echo "script execution ended at $END_TIME"
+
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo "script executed in $TOTAL_TIME"
