4d05f000db2343ebfc08a537c9b2296289dfab9d
rakesh <rakeshgautam602@gmail.com>
2026-09-07 10:28:33 +0530

    action

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -7,5 +7,5 @@ if [ $NUMBER -gt 20 ]; then
 elif [ $NUMBER -lt 20 ]; then
   echo "Given number: $NUMBER is less than 20"
 else
-  echo "Given number: $NUMBER is equal than 20"
+  echo "Given number: $NUMBER is equal to 20"
 fi
--- 10-installation.sh
+++ 10-installation.sh
@@ -1,18 +1,2 @@
 #! /bin/bash
 
-USER_ID=$(id -u)
-
-if [ $USER_ID -ne 0 ]; then
-   echo "run the script as root user"
-   exit 1
-fi
-
-echo "Installing Nginx"
-dnf install nginx -y
-
-if [ $? -ne 0 ]; then
-  echo "Nginx installation failure"
-  exit 1
-else
-  echo "Nginx installed successfully"
-fi
