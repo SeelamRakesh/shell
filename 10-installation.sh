afc6d8772a13779c9aae36fb4d023568a80ce547
rakesh <rakeshgautam602@gmail.com>
2026-09-07 10:32:56 +0530

    action

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,2 +1,18 @@
 #! /bin/bash
 
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+  echo "Run the script as Root user"
+  exit 1
+fi
+
+echo "Installing Nginx"
+dnf install nginx -y
+
+if [ $? -eq 0 ]; then
+  echo "Nginx installation successful"
+else
+  echo "Nginx installation failure"
+fi
+
