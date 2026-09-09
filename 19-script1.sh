a5082f0035626ab86770ea99258d6a1b541e2c77
rakesh <rakeshgautam602@gmail.com>
2026-09-09 18:25:34 +0530

    actions

---
+++ 19-script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COUNTRY=INDIA
+
+echo "I am in country: $COUNTRY"
+echo "PID of script-1: $$"
+
+sh 20-script2.sh
---
+++ 20-script2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COUNTRY=AMERICA
+
+echo "I am in country: $COUNTRY"
+echo "PID of script-1: $$"
