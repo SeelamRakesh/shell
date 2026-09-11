b83acd648005140e95b9abb5ba71ecd90e79659f
rakesh <rakeshgautam602@gmail.com>
2026-09-11 19:09:53 +0530

    actions

--- 14-deleting-logs.sh
+++ 14-deleting-logs.sh
@@ -13,7 +13,7 @@ if [ ! -d $LOG_DIR ]; then
   exit 1
 fi
 
-FILES_TO_DELETE=$(find app_logs -name "*.logs" -mtime +14)
+FILES_TO_DELETE=$(find $LOG_DIR -name "*.logs" -mtime +14)
 
 while IFS= read -r filepath ; do 
   echo $filepath
