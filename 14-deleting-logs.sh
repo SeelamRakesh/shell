5ad43ed73d696a05b325d9a375c8952c87b1fd71
rakesh <rakeshgautam602@gmail.com>
2026-09-11 19:04:33 +0530

    actions

--- 14-deleting-logs.sh
+++ 14-deleting-logs.sh
@@ -5,8 +5,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG_DIR="/home/ec2-user/app_logs"
-LOG_FILE="$LOG_DIR/$0.log"
+LOG_DIR=/home/ec2-user/app_logs
+LOG_FILE=$LOG_DIR/$0.log
 
 if [ ! -d $LOG_DIR ]; then
   echo "Directory doesn't exist"
