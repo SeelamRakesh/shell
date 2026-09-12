df75087209dac2f17a907dce13dec32069090346
rakesh <rakeshgautam602@gmail.com>
2026-09-12 21:36:13 +0530

    actions

--- 22-disk-utilisation.sh
+++ 22-disk-utilisation.sh
@@ -5,27 +5,14 @@ USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do
-   USAGE=$(echo $line | awk '{print $6}' | cut -d '%' -f1)
-   PARTITION=$(echo $line | awk '{print $7}')
-   
-   if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-     MESSAGE+="High Disk usage on $PARTITION: $USAGE% <br>"
-    fi
-done <<< $DISK_USAGE
-
-echo -e "$MESSAGE"
-
-DISK_USAGE=$(df -hT | grep -v Filesystem)
-USAGE_THRESHOLD=3
-
-while IFS= read -r line
-do
-    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk '{print $6}' | cut '%' -f1)
     PARTITION=$(echo $line | awk '{print $7}')
 
-    if [ "$USAGE" -ge "$USAGE_THRESHOLD" ]; then
-        MESSAGE+="High Disk usage on $PARTITION: $USAGE% \n"
+    if [ $USAGE_THRESHOLD = $USAGE ]; then
+      MESSAGE+="High Disk Usage on $PARTITION: $USAGE% <br>"
     fi
 done <<< $DISK_USAGE
 
 echo -e "$MESSAGE"
+
+sh mail.sh "rakeshgautam602@gmail.com" "High Disk Usage Alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DevOps Team"
