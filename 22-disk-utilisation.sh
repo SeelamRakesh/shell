52de6a45e90b64b84e5be436f5df837aadb69415
rakesh <rakeshgautam602@gmail.com>
2026-09-12 19:23:03 +0530

    actions

--- 22-disk-utilisation.sh
+++ 22-disk-utilisation.sh
@@ -5,8 +5,8 @@ USAGE_THRESHOLD=3
 
 while IFS= read -r line
 do
-   USAGE=$(grep -v $line | awk '{print $6}' | cut -d '%' -f1)
-   PARTITION=$(grep -v $line | awk '{print $7}')
+   USAGE=$(echo $line | awk '{print $6}' | cut -d '%' -f1)
+   PARTITION=$(echo $line | awk '{print $7}')
    echo "USAGE"
    echo "PARTITION"
 done <<< $DISK_USAGE
